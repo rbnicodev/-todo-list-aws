4d1b664e12bf58f7ae6f84e6dc4991aef6a42322
rbnicodev <rubenmsanchis.dev@gmail.com>
2023-06-10 23:07:00 +0000

    test rollback with errors

--- src/list.py
+++ src/list.py
@@ -8,7 +8,8 @@ def list(event, context):
     result = todoList.get_items()
     # create a response
     response = {
-        "statusCode": 200,
+        "statusCode": 500,
         "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
     }
+    
     return response
