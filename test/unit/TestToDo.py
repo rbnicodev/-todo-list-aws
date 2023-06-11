96e32009408698fabab6bde5220a5dc314326947
rbnicodev <rubenmsanchis.dev@gmail.com>
2023-06-11 20:26:20 +0000

    add get_todo_error test

--- test/unit/TestToDo.py
+++ test/unit/TestToDo.py
@@ -103,6 +103,16 @@ class TestDatabaseFunctions(unittest.TestCase):
             self.text,
             responseGet['text'])
         print ('End: test_get_todo')
+        
+    def test_get_todo_error(self):
+        print ('---------------------')
+        print ('Start: test_get_todo_error')
+        # Testing file functions
+        from src.todoList import get_item
+        # Table mock
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        self.assertRaises(Exception, get_item("", self.dynamodb))
+        print ('End: test_get_todo_error')
     
     def test_list_todo(self):
         print ('---------------------')
